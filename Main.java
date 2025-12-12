d807334cf19d7071f84e4a7bd918796de3ee74ca
alfatih <muhammadalfatihwibowo8@gmail.com>
2025-12-12 08:53:21 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        PegawaiTetap tetap = new PegawaiTetap("Budi", 30, "Manager");
+        PegawaiKontrak kontrak = new PegawaiKontrak("Sari", 25, "Staff");
+
+        System.out.println("=== Pegawai Tetap ===");
+        System.out.println("Nama: " + tetap.getNama());
+        System.out.println("Usia: " + tetap.getUsia());
+        System.out.println("Jabatan: " + tetap.getJabatan());
+        tetap.deskripsiKerja();
+
+        System.out.println("\n=== Pegawai Kontrak ===");
+        System.out.println("Nama: " + kontrak.getNama());
+        System.out.println("Usia: " + kontrak.getUsia());
+        System.out.println("Jabatan: " + kontrak.getJabatan());
+        kontrak.deskripsiKerja();
+    }
+}
